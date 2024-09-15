1b7ff33270074770d16555e99f020ef31d435104
khannghoang <hok74554@gmail.com>
2024-09-15 18:33:11 +0700

    added navbar and hero sections

---
+++ src/app/(front)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../../../components/Frontend/Navbar";
+import React, { ReactNode } from "react"
+export default function layout({children} : {children:ReactNode}){
+    return(
+        <div>
+            <Navbar/>
+            {children}
+        </div>
+    )
+}
