9363a73c8d072f84852512f3dc9383a6ed08d822
khannghoang <hok74554@gmail.com>
2024-09-15 18:51:23 +0700

    Change for deloy and deloy to vercel

--- components/Frontend/Navbar.tsx
+++ components/Frontend/Navbar.tsx
@@ -4,24 +4,24 @@ import { useState } from 'react'
 import {
   Dialog,
   DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
+  // Disclosure,
+  // DisclosureButton,
+  // DisclosurePanel,
+  // Popover,
+  // PopoverButton,
   PopoverGroup,
-  PopoverPanel,
+  // PopoverPanel,
 } from '@headlessui/react'
 import {
-  ArrowPathIcon,
+  // ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
+  // ChartPieIcon,
+  // CursorArrowRaysIcon,
+  // FingerPrintIcon,
+  // SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+// import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 
 export default function Navbar() {
