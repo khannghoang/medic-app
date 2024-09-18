f267c9176be59ae70848ec3a34d854030df4e8e8
khannghoang <hok74554@gmail.com>
2024-09-18 16:50:41 +0700

    Change

--- components/Frontend/Navbar.tsx
+++ components/Frontend/Navbar.tsx
@@ -28,8 +28,8 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-blue-950">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
+    <header className="bg-blue-950 border-b border-gray-400/60">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8 ">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
@@ -65,7 +65,7 @@ export default function Navbar() {
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-blue-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-blue-400 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
---
+++ notes.md
@@ -0,0 +1,6 @@
+Book you Occupational Therapist
+Acupuncturist
+no insurance hassle
+Health shouldn't be a puzzle, we are cutting through the B.S to bring you simple, affordable, and transaparent healthcare
+const TEXTS = ['Acupuncture', 'Massage', 'Chiropractor', "Dental",
+"Cosmetic", "Dietitian", "Speech Therapist", "Occupational, Therapist", " Acupuncturist"];
